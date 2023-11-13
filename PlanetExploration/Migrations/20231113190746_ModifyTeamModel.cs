bde153415c4586eae18a40f28ba009541073ec33
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-13 21:08:43 +0200

    Configure Team Model such that a team can visit multiple planets

---
+++ PlanetExploration/Migrations/20231113190746_ModifyTeamModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlanetExploration.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyTeamModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- PlanetExploration/PlanetExploration.Core/Models/Team.cs
+++ PlanetExploration/PlanetExploration.Core/Models/Team.cs
@@ -5,5 +5,6 @@
         public int Id { get; set; }
         public virtual HumanCaptain HumanCaptain { get; set; }
         public virtual ICollection<Robot> Robots { get; set; } = new HashSet<Robot>();
+        public virtual ICollection<Planet> Planets { get; set; } = new HashSet<Planet>();
     }
 }
