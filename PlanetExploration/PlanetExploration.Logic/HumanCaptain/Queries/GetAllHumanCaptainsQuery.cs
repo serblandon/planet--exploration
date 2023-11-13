7037578f9cb42794f1c1313165ed53ccbaa273b0
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-13 14:03:35 +0200

    Implement query and query handler for HumanCaptain

---
+++ PlanetExploration/PlanetExploration.Logic/HumanCaptain/Queries/GetAllHumanCaptainsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace PlanetExploration.PlanetExploration.Logic.HumanCaptain.Queries
+{
+    public class GetAllHumanCaptainsQuery : IRequest<IEnumerable<Core.Models.HumanCaptain>>
+    {
+    }
+}
---
+++ PlanetExploration/PlanetExploration.Logic/HumanCaptain/Queries/GetAllHumanCaptainsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PlanetExploration.PlanetExploration.Core.Models;
+using PlanetExploration.PlanetExploration.Dal.Data;
+
+namespace PlanetExploration.PlanetExploration.Logic.HumanCaptain.Queries
+{
+    public class GetAllHumanCaptainsQueryHandler : IRequestHandler<GetAllHumanCaptainsQuery, IEnumerable<Core.Models.HumanCaptain>>
+    {
+        private readonly PlanetExplorationContext _planetExplorationContext;
+        public GetAllHumanCaptainsQueryHandler(PlanetExplorationContext planetExplorationContext)
+        {
+            _planetExplorationContext = planetExplorationContext;
+        }
+
+        public async Task<IEnumerable<Core.Models.HumanCaptain>> Handle(GetAllHumanCaptainsQuery request, CancellationToken cancellationToken)
+        {
+            return await _planetExplorationContext.HumanCaptains.ToListAsync(cancellationToken);
+        }
+    }
+}
