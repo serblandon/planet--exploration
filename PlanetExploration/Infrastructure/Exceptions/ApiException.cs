4489e25b8559947a485730db16f82a76c90e24a8
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-15 12:14:32 +0200

    Implement custom ApiException

---
+++ PlanetExploration/Infrastructure/Exceptions/ApiException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace PlanetExploration.Infrastructure.Exceptions
+{
+    public class ApiException : Exception
+    {
+        public ApiException()
+        {
+        }
+
+        public ApiException(string message) : base(message)
+        {
+        }
+
+        public ApiException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
+
