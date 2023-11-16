d17bee311f214fd43cd8bb2b0c2da7f78d943092
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-16 11:08:38 +0200

    Add query and handler for getplanets

---
+++ PlanetExploration/PlanetExploration.Logic/Planet/Queries/GetAllPlanets/GetAllPlanetsQuery.cs
@@ -0,0 +1,8 @@
+﻿using MediatR;
+
+namespace PlanetExploration.PlanetExploration.Logic.Planet.Queries.GetAllPlanets
+{
+    public class GetAllPlanetsQuery : IRequest<IEnumerable<Core.Models.Planet>>
+    {
+    }
+}
---
+++ PlanetExploration/PlanetExploration.Logic/Planet/Queries/GetAllPlanets/GetAllPlanetsQueryHandler.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PlanetExploration.PlanetExploration.Dal.Data;
+
+namespace PlanetExploration.PlanetExploration.Logic.Planet.Queries.GetAllPlanets
+{
+    public class GetAllPlanetsQueryHandler : IRequestHandler<GetAllPlanetsQuery, IEnumerable<Core.Models.Planet>>
+    {
+        private readonly PlanetExplorationContext _planetExplorationContext;
+        public GetAllPlanetsQueryHandler(PlanetExplorationContext planetExplorationContext)
+        {
+            _planetExplorationContext = planetExplorationContext;
+        }
+        public async Task<IEnumerable<Core.Models.Planet>> Handle(GetAllPlanetsQuery request, CancellationToken cancellationToken)
+        {
+            return await _planetExplorationContext.Planets.ToListAsync(cancellationToken);
+        }
+    }
+}
