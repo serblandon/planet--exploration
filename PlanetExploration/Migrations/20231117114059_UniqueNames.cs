e98944413434a136b859feffe0e4d72d5bd10e97
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-17 13:41:27 +0200

    Add unique names db constraint

---
+++ PlanetExploration/Migrations/20231117114059_UniqueNames.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlanetExploration.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniqueNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Robots",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "HumanCaptains",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Robots_Name",
+                table: "Robots",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HumanCaptains_Name",
+                table: "HumanCaptains",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Robots_Name",
+                table: "Robots");
+
+            migrationBuilder.DropIndex(
+                name: "IX_HumanCaptains_Name",
+                table: "HumanCaptains");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Robots",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "HumanCaptains",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
