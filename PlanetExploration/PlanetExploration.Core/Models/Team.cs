c53a7ca5a4a6a7e1c591fb43ff47c754b26d3a12
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-17 16:07:20 +0200

    Update Models without using Team

--- PlanetExploration/PlanetExploration.Core/Models/Team.cs
+++ PlanetExploration/PlanetExploration.Core/Models/Team.cs
@@ -3,6 +3,7 @@
     public class Team
     {
         public int Id { get; set; }
+        public int HumanCaptainId { get; set; }
         public virtual HumanCaptain HumanCaptain { get; set; }
         public virtual ICollection<Robot> Robots { get; set; } = new HashSet<Robot>();
         public virtual ICollection<Planet> Planets { get; set; } = new HashSet<Planet>();
