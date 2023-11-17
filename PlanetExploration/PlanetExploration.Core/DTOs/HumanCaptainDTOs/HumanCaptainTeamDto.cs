21c5d598582d4be67d72fdae43fd1561265c0d17
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-17 14:09:48 +0200

    Add Team Dtos for Robot and captain

---
+++ PlanetExploration/PlanetExploration.Core/DTOs/HumanCaptainDTOs/HumanCaptainTeamDto.cs
@@ -0,0 +1,17 @@
+﻿using PlanetExploration.PlanetExploration.Core.Models;
+
+namespace PlanetExploration.PlanetExploration.Core.DTOs.HumanCaptainDTOs
+{
+    public class HumanCaptainTeamDto
+    {
+        public int Id { get; set; }
+
+        public static implicit operator HumanCaptain(HumanCaptainTeamDto entity)
+        {
+            return new()
+            {
+                Id = entity.Id
+            };
+        }
+    }
+}
