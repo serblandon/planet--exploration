a5a63bd247881842e053512877bd12d9013db416
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-17 16:44:52 +0200

    Modify planet entity to necesities and implemetn add functionality

---
+++ PlanetExploration/PlanetExploration.Logic/Planet/Commands/AddPlanet/AddPlanetCommand.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using PlanetExploration.PlanetExploration.Core.DTOs.PlanetDTOs;
+
+namespace PlanetExploration.PlanetExploration.Logic.Planet.Commands.AddPlanet
+{
+    public class AddPlanetCommand : IRequest<Core.Models.Planet>
+    {
+        public PlanetDto Planet { get; set; }
+    }
+}
---
+++ PlanetExploration/PlanetExploration.Logic/Planet/Commands/AddPlanet/AddPlanetCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using PlanetExploration.Infrastructure.Exceptions;
+using PlanetExploration.PlanetExploration.Dal.Data;
+
+namespace PlanetExploration.PlanetExploration.Logic.Planet.Commands.AddPlanet
+{
+    public class AddPlanetCommandHandler : IRequestHandler<AddPlanetCommand, Core.Models.Planet>
+    {
+        private readonly PlanetExplorationContext _planetExplorationContext;
+        public AddPlanetCommandHandler(PlanetExplorationContext planetExplorationContext)
+        {
+            _planetExplorationContext = planetExplorationContext;
+        }
+        public async Task<Core.Models.Planet> Handle(AddPlanetCommand request, CancellationToken cancellationToken)
+        {
+            var alreadyInDataBase = await _planetExplorationContext.Planets.FirstOrDefaultAsync(u => u.Name == request.Planet.Name, cancellationToken);
+            if (alreadyInDataBase != null)
+            {
+                throw new ApiException($"Planet with name {request.Planet.Name} already exists.");
+            }
+
+            Core.Models.Planet entity = request.Planet;
+
+            await _planetExplorationContext.Planets.AddAsync(entity, cancellationToken);
+            await _planetExplorationContext.SaveChangesAsync(cancellationToken);
+
+            return entity;
+        }
+    }
+}
