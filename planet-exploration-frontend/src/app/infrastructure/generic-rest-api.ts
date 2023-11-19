70caf1aba56383c5008251fccc7d6f4ee13ff785
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-19 22:36:47 +0200

    remove api response dependency in generic rest api

--- planet-exploration-frontend/src/app/infrastructure/generic-rest-api.ts
+++ planet-exploration-frontend/src/app/infrastructure/generic-rest-api.ts
@@ -1,35 +1,31 @@
 import { HttpClient } from "@angular/common/http";
-import { Observable, map } from "rxjs";
-import { IApiResponse } from "./api-response";
+import { Observable } from "rxjs";
 
 export class GenericRestApi<T> {
     protected url = '';
     constructor(protected http: HttpClient, private controller: string = '') {
-        this.url = this.getUrlForModel();
+        this.url = `/${this.controller}`;
     }
   
     public getAll(): Observable<T[]> {
-      return this.http.get<IApiResponse<T[]>>(this.url).pipe(map(x => x.data));
+      return this.http.get<T[]>(this.url);
     }
+    
   
     public getById(id: number): Observable<T> {
-      return this.http.get<IApiResponse<T>>(`${this.url}/${id}`).pipe(map(x => x.data));
+      return this.http.get<T>(`${this.url}/${id}`);
     }
   
     public add(data: T): Observable<T> {
-      return this.http.post<IApiResponse<T>>(this.url, data).pipe(map(x => x.data));
+      return this.http.post<T>(this.url, data);
     }
   
-    public update(data: T): Observable<IApiResponse<T>> {
-      return this.http.put<IApiResponse<T>>(this.url, data);
+    public update(data: T): Observable<T> {
+      return this.http.put<T>(this.url, data);
     }
 
     public deleteById(id: number): Observable<void> {
         const url = `${this.url}/${id}`;
         return this.http.delete<void>(url);
-      }
-
-    private getUrlForModel(): string {
-        return `/${this.controller}`;
       }
 }
