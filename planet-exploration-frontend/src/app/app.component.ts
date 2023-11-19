d690c47e935f15d3b5d5223d080f8c07317ab001
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-19 20:07:13 +0200

    Update app component so that it displays planets

--- planet-exploration-frontend/src/app/app.component.ts
+++ planet-exploration-frontend/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IPlanet } from './interfaces/planet';
+import { PlanetService } from './services/planet.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
+
+  planets: IPlanet[] = [];
+
+  constructor(private planetService: PlanetService) {}
+
+  ngOnInit(): void {
+    this.planetService.getAllPlanets().subscribe({
+      next: (data: IPlanet[]) => {
+        this.planets = data;
+      },
+      error: (error) => {
+        console.error('Error fetching planets:', error);
+      }
+    });
+  }
   title = 'planet-exploration-frontend';
 }
--- planet-exploration-frontend/src/app/app.module.ts
+++ planet-exploration-frontend/src/app/app.module.ts
@@ -3,16 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlanetComponent } from './components/planet/planet.component';
+import { PlanetService } from './services/planet.service';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlanetComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [PlanetService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
