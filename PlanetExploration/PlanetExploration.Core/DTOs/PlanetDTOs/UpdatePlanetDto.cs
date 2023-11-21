526869ae7b0a661d8f3551888857735064234ba9
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-21 12:46:08 +0200

    Update planetdto for put request

--- PlanetExploration/PlanetExploration.Core/DTOs/PlanetDTOs/UpdatePlanetDto.cs
+++ PlanetExploration/PlanetExploration.Core/DTOs/PlanetDTOs/UpdatePlanetDto.cs
@@ -1,4 +1,5 @@
 ﻿using PlanetExploration.PlanetExploration.Core.DTOs.HumanCaptainDTOs;
+using PlanetExploration.PlanetExploration.Core.DTOs.RobotDTOs;
 using PlanetExploration.PlanetExploration.Core.Models;
 
 namespace PlanetExploration.PlanetExploration.Core.DTOs.PlanetDTOs
@@ -7,16 +8,19 @@ namespace PlanetExploration.PlanetExploration.Core.DTOs.PlanetDTOs
     {
         public string Description { get; set; }
         public PlanetStatus Status { get; set; }
-        public HumanCaptainPlanetDto HumanCaptain { get; set; }
+        public virtual HumanCaptainPlanetDto HumanCaptain { get; set; }
+        public virtual ICollection<AddRobotDto> Robots { get; set; }
 
         public static implicit operator Planet(UpdatePlanetDto entity)
         {
+
             return new()
             {
                 Description = entity.Description,
                 Status = entity.Status,
                 HumanCaptain = entity.HumanCaptain,
-            };
+                Robots = entity.Robots.Select(robotentity => (Robot)robotentity).ToList()
+        };
         }
     }
 }
