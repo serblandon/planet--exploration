9424764b310e50a2b06c3a71d5fe731af617c09c
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-21 12:44:05 +0200

    Add new humancaptain dto

---
+++ PlanetExploration/PlanetExploration.Core/DTOs/HumanCaptainDTOs/HumanCaptainPlanetDto.cs
@@ -0,0 +1,18 @@
+﻿using PlanetExploration.PlanetExploration.Core.Models;
+
+namespace PlanetExploration.PlanetExploration.Core.DTOs.HumanCaptainDTOs
+{
+    public class HumanCaptainPlanetDto
+    {
+        public string Name { get; set; }
+
+
+        public static implicit operator HumanCaptain(HumanCaptainPlanetDto entity)
+        {
+            return new()
+            {
+                Name = entity.Name,
+            };
+        }
+    }
+}
