a9ecb9e4878e72bf9e605b20fd5d35e07800ed91
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-26 21:41:20 +0200

    Import reactive forms module

--- planet-exploration-frontend/src/app/app.module.ts
+++ planet-exploration-frontend/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HttpClientModule } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxSpinnerModule } from 'ngx-spinner';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -20,7 +21,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     HttpClientModule,
     NgxSpinnerModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [PlanetService],
   bootstrap: [AppComponent]
