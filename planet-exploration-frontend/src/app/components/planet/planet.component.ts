a95c5c2383ccfdc9e0ee3643e4613a2adad27f8c
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-26 17:28:07 +0200

    Remove unused import

--- planet-exploration-frontend/src/app/components/planet/planet.component.ts
+++ planet-exploration-frontend/src/app/components/planet/planet.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { IPlanet } from 'src/app/interfaces/planet';
-import { RobotListPipe } from 'src/app/pipes/robot-list.pipe';
 import { PlanetService } from 'src/app/services/planet.service';
 @Component({
   selector: 'app-planet',
