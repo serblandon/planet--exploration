9c60acaa1a4878b037c8ee9ab67e303e236a6a6f
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-26 21:40:47 +0200

    Implement robot and humancaptain services

--- planet-exploration-frontend/src/app/services/human-captain.service.ts
+++ planet-exploration-frontend/src/app/services/human-captain.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { GenericRestApi } from '../infrastructure/generic-rest-api';
+import { IHumanCaptain } from '../interfaces/human-captain';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class HumanCaptainService {
+export class HumanCaptainService extends GenericRestApi<IHumanCaptain>{
 
-  constructor() { }
+  constructor(protected override http: HttpClient) {
+    super(http, "api/HumanCaptain");
+  }
 }
--- planet-exploration-frontend/src/app/services/robot.service.ts
+++ planet-exploration-frontend/src/app/services/robot.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { GenericRestApi } from '../infrastructure/generic-rest-api';
+import { IRobot } from '../interfaces/robot';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
-export class RobotService {
+export class RobotService extends GenericRestApi<IRobot>{
 
-  constructor() { }
+  constructor(protected override http: HttpClient) {
+    super(http, "api/Robot");
+  }
 }
