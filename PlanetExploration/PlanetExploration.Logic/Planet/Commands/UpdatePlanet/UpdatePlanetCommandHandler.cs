11225d86f035dc88e244263c2278415646b39992
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-27 22:47:59 +0200

    Add Transaction logic for update planet

--- PlanetExploration/PlanetExploration.Logic/Planet/Commands/UpdatePlanet/UpdatePlanetCommandHandler.cs
+++ PlanetExploration/PlanetExploration.Logic/Planet/Commands/UpdatePlanet/UpdatePlanetCommandHandler.cs
@@ -17,61 +17,74 @@ namespace PlanetExploration.PlanetExploration.Logic.Planet.Commands.UpdatePlanet
         }
         public async Task<Unit> Handle(UpdatePlanetCommand request, CancellationToken cancellationToken)
         {
-            if (request.Planet.Status == PlanetStatus.EnRoute)
-            {
-                throw new ApiException($"This planet has been explored; a status of OK or NOTOK is expected.");
-            }
+            using var transaction = await _planetExplorationContext.Database.BeginTransactionAsync(cancellationToken);
 
-            var planet = await _planetExplorationContext.Planets
-                .Include(p => p.HumanCaptain)
-                .FirstOrDefaultAsync(u => u.Id == request.Id, cancellationToken);
-            if (planet == null)
+            try
             {
-                throw new ApiException($"Planet with id: {request.Id} not found!");
-            }
-            if (planet.Status is not 0)
-            {
-                throw new ApiException($"Planet with id: {request.Id} has already been visited!");
-            }
-
-            var humanCaptain = await _planetExplorationContext.HumanCaptains.FirstOrDefaultAsync(h => h.Name == request.Planet.HumanCaptain.Name, cancellationToken);
-            if (humanCaptain == null)
-            {
-                throw new ApiException($"Human Captain with name: {request.Planet.HumanCaptain.Name} not found!");
-            }
-
-            // get all requested robot names
-            var robotNames = request.Planet.Robots.Select(r => r.Name).ToList();
-            // get all matching robot names from DB
-            var robots = await _planetExplorationContext.Robots
-                .Where(r => robotNames.Contains(r.Name))
-                .ToListAsync(cancellationToken);
-            // check if any of the requested robot with a name doesn't exist
-            foreach (var requestedRobotName in robotNames)
-            {
-                if (!robots.Any(r => r.Name == requestedRobotName))
+                if (request.Planet.Status == PlanetStatus.EnRoute)
                 {
-                    throw new ApiException($"Robot with Name: {requestedRobotName} not found!");
+                    throw new ApiException($"This planet has been explored; a status of OK or NOTOK is expected.");
                 }
-            }
 
-            if (!string.IsNullOrEmpty(request.Planet.Status.ToString()))
-            {
-                planet.Status = request.Planet.Status;
-            }
-            if (!string.IsNullOrEmpty(request.Planet.Description))
-            {
-                planet.Description = request.Planet.Description;
-            }
+                var planet = await _planetExplorationContext.Planets
+                    .Include(p => p.HumanCaptain)
+                    .FirstOrDefaultAsync(u => u.Id == request.Id, cancellationToken);
+                if (planet == null)
+                {
+                    throw new ApiException($"Planet with id: {request.Id} not found!");
+                }
+                if (planet.Status is not 0)
+                {
+                    throw new ApiException($"Planet with id: {request.Id} has already been visited!");
+                }
 
-            foreach (var robot in robots)
-            {
-                robot.PlanetId = planet.Id;
+                var humanCaptain = await _planetExplorationContext.HumanCaptains.FirstOrDefaultAsync(h => h.Name == request.Planet.HumanCaptain.Name, cancellationToken);
+                if (humanCaptain == null)
+                {
+                    throw new ApiException($"Human Captain with name: {request.Planet.HumanCaptain.Name} not found!");
+                }
+
+                // get all requested robot names
+                var robotNames = request.Planet.Robots.Select(r => r.Name).ToList();
+                // get all matching robot names from DB
+                var robots = await _planetExplorationContext.Robots
+                    .Where(r => robotNames.Contains(r.Name))
+                    .ToListAsync(cancellationToken);
+                // check if any of the requested robot with a name doesn't exist
+                foreach (var requestedRobotName in robotNames)
+                {
+                    if (!robots.Any(r => r.Name == requestedRobotName))
+                    {
+                        throw new ApiException($"Robot with Name: {requestedRobotName} not found!");
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(request.Planet.Status.ToString()))
+                {
+                    planet.Status = request.Planet.Status;
+                }
+                if (!string.IsNullOrEmpty(request.Planet.Description))
+                {
+                    planet.Description = request.Planet.Description;
+                }
+
+                foreach (var robot in robots)
+                {
+                    robot.PlanetId = planet.Id;
+                }
+                humanCaptain.PlanetId = planet.Id;
+
+                _planetExplorationContext.Planets.Update(planet);
+                await _planetExplorationContext.SaveChangesAsync(cancellationToken);
+
+                await transaction.CommitAsync(cancellationToken);
             }
-            humanCaptain.PlanetId = planet.Id;
-            
-            _planetExplorationContext.Planets.Update(planet);
-            await _planetExplorationContext.SaveChangesAsync(cancellationToken);
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync(cancellationToken);
+
+                throw new ApiException("An error occurred while updating the planet.", ex);
+            }
 
             return Unit.Value;
         }
