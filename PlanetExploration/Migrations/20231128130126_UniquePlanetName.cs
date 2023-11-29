ad118c282756b730c50cd20986dbe328e94aa42d
Serban Crasovan <serban.crasovan@student.upt.ro>
2023-11-29 21:04:09 +0200

    Add latest migrations

---
+++ PlanetExploration/Migrations/20231128130126_UniquePlanetName.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PlanetExploration.Migrations
+{
+    /// <inheritdoc />
+    public partial class UniquePlanetName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Planets",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Planets",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Planets_Name",
+                table: "Planets",
+                column: "Name",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Planets_Name",
+                table: "Planets");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Planets");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Planets",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+    }
+}
